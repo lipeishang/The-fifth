741155964e1ba32ff47a300eaa6100cb14ecf0e5
lipeishang <1315789454@qq.com>
2016-05-22 12:47:56 +0800

    thefifth

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,14 @@
-'use strict';
-
 function double_to_one(collection) {
+  var result = [];
 
-  //在这里写入代码
+  result.push(collection[0]);
+  collection.forEach(function (element) {
+    for(var i = 0; i < element.length; i++){
+      result.push(element[i]);
+    }
+  });
+
+  return result;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,39 @@
-'use strict';
-
 function double_to_one(collection) {
-
-  //在这里写入代码
+  var oneDimensionalArr = transition(collection) ;
+  return eliminateRepeat(oneDimensionalArr);
 }
+
+function transition(collection) {
+  var oneDimensionalArr = [];
+
+  collection.forEach(function (element) {
+    for(var i = 0; i < element.length;i++){
+      oneDimensionalArr.push(element[i]);
+    }
+  });
+
+  return oneDimensionalArr;
+}
+
+function eliminateRepeat(oneDimensionalArr) {
+  var noRepeatArr = [];
+  var flag = 0;
+
+  noRepeatArr.push(oneDimensionalArr[0]);
+  for (var i = 1; i < oneDimensionalArr.length; i++) {
+    for (var j = 0; j < noRepeatArr.length; j++) {
+      if (oneDimensionalArr[i] === noRepeatArr[j]) {
+        flag = 1;
+      }
+    }
+    if (flag === 0) {
+      noRepeatArr.push(oneDimensionalArr[i]);
+    }
+    flag = 0;
+  }
+
+  return noRepeatArr;
+}
+
 
 module.exports = double_to_one;
